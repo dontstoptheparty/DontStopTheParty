42ec15d7e3be239a860da42c8f3a438f1e78450c
Wellington Espindula <wellington.m.espindula@gmail.com>
2020-11-18 15:50:35 -0300

    Revert "Version 1.0.1 - Removing bad smells from static code analysis and Copyrighting the code"
    This reverts commit 9995f060

--- src/main/java/br/ufrgs/inf/dontstoptheparty/App.java
+++ src/main/java/br/ufrgs/inf/dontstoptheparty/App.java
@@ -3,11 +3,6 @@
  * Designed by Guilherme Santana, João Pedro Silveira, Renan Magagnin e Wellington M. Espindula.
  */
 
-/*
- * DontStopTheParty 2020.
- * Designed by Guilherme Santana, João Pedro Silveira, Renan Magagnin e Wellington M. Espindula.
- */
-
 package br.ufrgs.inf.dontstoptheparty;
 
 import br.ufrgs.inf.dontstoptheparty.ui.TextMainUI;
--- src/main/java/br/ufrgs/inf/dontstoptheparty/song/player/Player.java
+++ src/main/java/br/ufrgs/inf/dontstoptheparty/song/player/Player.java
@@ -3,11 +3,6 @@
  * Designed by Guilherme Santana, João Pedro Silveira, Renan Magagnin e Wellington M. Espindula.
  */
 
-/*
- * DontStopTheParty 2020.
- * Designed by Guilherme Santana, João Pedro Silveira, Renan Magagnin e Wellington M. Espindula.
- */
-
 package br.ufrgs.inf.dontstoptheparty.song.player;
 
 import br.ufrgs.inf.dontstoptheparty.song.SongState;
